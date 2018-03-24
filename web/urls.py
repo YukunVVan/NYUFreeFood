707eb0a6a45edad663fc2c11bd24c5705dd5b588
Jack Tsai <b00602010@ntu.edu.tw>
2018-03-24 14:39:58 -0400

    web

---
+++ web/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'web.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.index),
+    url(r'^admin/', admin.site.urls),
+
+]
